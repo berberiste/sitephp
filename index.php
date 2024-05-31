2b96688734549a0f8f166dd1a444a614bb6fecc9
berberiste <134873162+berberiste@users.noreply.github.com>
2024-05-31 15:43:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
+<?php
+echo "<h1>hellow i am a PHP code</h1>";
+echo "<h1>here you can write a code PHP</h1>";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <h1>hellow i am an HTML code</h1>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
